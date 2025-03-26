fd37b64c48ddce6da9a4d5d79949772f86965061
Silvan Gümüsdere <silvan@trollbox.org>
2025-03-26 17:02:45 +0100

    mark `processTemplate` as `inline` to make it include in multiple files without linker getting a meltdown

--- include/template.h
+++ include/template.h
@@ -4,8 +4,7 @@
 #ifndef ESP_SOLAR_BOY_TEMPLATE_H
 #define ESP_SOLAR_BOY_TEMPLATE_H
 
-
-String processTemplate(const String &templateContent, const std::map<String, String> &variables) {
+inline String processTemplate(const String &templateContent, const std::map<String, String> &variables) {
     String result = templateContent;
 
     for (const auto &pair: variables) {
