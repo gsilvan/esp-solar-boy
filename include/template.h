183cd33aeaa53128379990df44fdc49982e02bda
Silvan Gümüsdere <silvan@trollbox.org>
2025-03-28 12:10:56 +0100

    pre-allocate String memory

--- include/template.h
+++ include/template.h
@@ -1,10 +1,19 @@
 #ifndef ESP_SOLAR_BOY_TEMPLATE_H
 #define ESP_SOLAR_BOY_TEMPLATE_H
+
 #include <Arduino.h>
 #include <map>
 
 inline String processTemplate(const String &templateContent, const std::map<String, String> &variables) {
-    String result = templateContent;
+    // Estimate final size to reduce reallocations
+    size_t estimatedSize = templateContent.length();
+    for (const auto &pair: variables) {
+        estimatedSize += pair.second.length() - (pair.first.length() + 4); // Account for {{key}} replacement
+    }
+
+    String result;
+    result.reserve(estimatedSize); // Preallocate memory
+    result = templateContent;
 
     for (const auto &pair: variables) {
         result.replace("{{" + pair.first + "}}", pair.second);
